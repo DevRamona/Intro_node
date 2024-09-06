492cf6d7134e84068d8ad6feae738dd22efc26df
DevRamona <r.ingabire@alustudent.com>
2024-09-06 10:36:07 +0200

    routing using express

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-import http from 'http';
-import fs from "fs"
-import _ from 'lodash';
+import express from "express";
 
-const server = http.createServer()
-
-server.on("request", (request, response) => {
-    const readable = fs.createReadStream("input.txt")
-    readable.on("data", chunk => {
-        response.write(chunk)
-    })
-    readable.on("end" , () => {
-        response.end()
-    } )
+const app = express();
+app.get("/", (request, response) => {
+    response.status(200).json({message:"Hello from the server side", app:"naptours"})
 })
-server.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+
+app.get("/about", (request, response) => {
+    response.status(200).json({message:"Hello from the about section", app:"test"})
+})
+const port = 3000
+app.listen(port, () =>{
+    console.log("appp running on the port") 
+
+})                                                                                                                                                                                                                                                                    
